22df899ce2ddc3a3b990fd748cdfc8507fc8a319
Marco A <hehemarcos300@gmail.com>
2024-03-13 15:15:05 -0400

    att

---
+++ src/main/java/com/mycompany/trabalho/Bean02.java
@@ -0,0 +1,10 @@
+package com.mycompany.trabalho;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "ct2")
+@SessionScoped
+public class Bean02 {
+    
+}
