5973009dc649fb96a2fbd4d063dbd1b1253935ba
Marco A <hehemarcos300@gmail.com>
2024-03-13 14:40:41 -0400

    add pom

---
+++ src/main/java/com/mycompany/trabalho/Bean01.java
@@ -0,0 +1,10 @@
+package com.mycompany.trabalho;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "ct")
+@SessionScoped
+public class Bean01 {
+    
+}
